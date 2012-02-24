26ad41f16e96079da41c10ddd9ca44f1ce2e6897
Ross Paffett <ross@stratomine.com>
2012-02-24 13:01:16 -0500

    Initial commit

---
+++ src/test/java/com/stratomine/bukkit/plugins/HomewardPluginTest.java
@@ -0,0 +1,14 @@
+package com.stratomine.bukkit.plugins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomewardPluginTest {
+	
+	@Test
+	public void testPlugin() {
+		fail("Not yet implemented");
+	}
+	
+}
